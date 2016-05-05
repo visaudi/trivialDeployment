54d19200d234ec10243c7e65d4be9e941b45f3f4
Ben Pardo <benpardo@Bens-MacBook-Pro-2.local>
2016-05-05 01:53:42 -0600

    Added a database test and made it pass added db to travis

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var mongoose = require('mongoose');
 var app = express();
+var Item = require('./item.js');
 
 var items = [];
 
@@ -9,9 +11,15 @@ app.post('/items', jsonParser, function(req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
-
-    items.push(req.body.items[0]);
-    res.status(201).json(items[items.length - 1]);
+    items.push(req.body);
+    Item.create(req.body, function(err, item) {
+        if (err || !item) {
+            console.error("Could not create item");
+            return;
+        }
+        console.log("Created favorite", item.name);
+        res.status(201).json(items[items.length - 1]);
+    });
 });
 
 exports.app = app;
--- test.js
+++ test.js
@@ -1,25 +1,57 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var mongoose = require('mongoose');
 
 var should = chai.should();
 
+mongoose.connect('mongodb://localhost/items');
+var Item = require('./item.js');
 var app = require('./index.js').app;
 var items = require('./index.js').items;
 chai.use(chaiHttp);
+describe('a minimum of work', function() {
 
-it('should add an item on POST', function(done) {
-    chai.request(app)
-        .post('/items')
-        .send({'items': ['Kale']})
-        .end(function(err, res) {
-            should.equal(err, null);
-            res.should.have.status(201);
-            res.should.be.json;
-            res.body.should.be.a('string');
-            res.body.should.equal('Kale');
-            items.should.be.a('array');
-            items.should.have.length(1);
-            items[0].should.be.a('string');
-            done();
+    describe('a little HTTP', function() {
+
+        it('should add an item on POST', function(done) {
+            chai.request(app)
+                .post('/items')
+                .send({'name': 'Kale'})
+                .end(function(err, res) {
+                    should.equal(err, null);
+                    res.should.have.status(201);
+                    res.should.be.json;
+                    res.body.name.should.be.a('string');
+                    res.body.name.should.equal('Kale');
+                    done();
+                });
         });
+
+    });
+
+    beforeEach(function(done) {
+        mongoose.connection.db.dropDatabase();
+        var broccoli = {
+            name: 'broccoli',
+        };
+
+
+        chai.request(app)
+            .post('/items')
+            .send(broccoli)
+            .end(done);
+
+
+    });
+
+    describe('barely a database', function() {
+
+        it('should have the same item in the db that we sent over the post', function(done) {
+            Item.findOne({name: "broccoli"}, function(err, item) {
+                item.name.should.equal("broccoli");
+                done();
+            });
+        });
+    });
+
 });
